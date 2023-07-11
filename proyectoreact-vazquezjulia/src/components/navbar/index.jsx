82d98b253913cace3c415d482a839e5e7190bc7a
Julia Eva <julia.vazquez289@gmail.com>
2023-07-10 23:08:10 -0300

    tercercommit

--- proyectoreact-vazquezjulia/src/components/navbar/index.jsx
+++ proyectoreact-vazquezjulia/src/components/navbar/index.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import cartwidget from '../cartwidget/index';
-import itemlistconteiner from '../itemlistcontainer';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import cartwidget from '../Cartwidget/index';
 
-const navbar = () => {
+function navbar() {
   return (
-    <div>
-      <itemlistconteiner></itemlistconteiner>
-      <cartwidget></cartwidget>
-    </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Uva y cuajo - Eventos y vinos</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home"> Inicio </Nav.Link>
+            <Nav.Link href="#features">Eventos</Nav.Link>
+            <Nav.Link href="#pricing">Productos</Nav.Link>
+            <Nav.Link href="#pricing">Contacto</Nav.Link>
+            <cartwidget></cartwidget>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
-export default navbar
+export default navbar;
